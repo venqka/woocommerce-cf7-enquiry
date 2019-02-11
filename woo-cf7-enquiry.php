4e72701b09aa0c2ee7859a53bec0b6ed44a3014c
venqka <venelinborisov5@gmail.com>
2019-02-11 09:27:57 +0200

    Fix typos

--- woo-cf7-enquiry.php
+++ woo-cf7-enquiry.php
@@ -2,9 +2,15 @@
 /*
 Plugin Name: WooCommerce Contact Form 7 Enquiry
 Plugin URI: devnecks.com
-Description: This plugin adds Contact Form 7 enquiry WooCommerce prosucts
+Description: This plugin adds Contact Form 7 enquiry to WooCommerce products
 Author: venqka
 Author URI: devnecks.com
 Version: 1.0
 */
 
+function wcfe_load_text_domain() {
+
+	load_plugin_textdomain( 'wcfe', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+
+}
+add_action( 'init', 'wcfe_load_text_domain' );

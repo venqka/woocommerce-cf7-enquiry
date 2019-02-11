2cc338ad121921730e53abefcb52bac9a5cf2b8b
venqka <venelinborisov5@gmail.com>
2019-02-11 09:07:14 +0200

    Load plugin text domain

---
+++ woo-cf7-enquiry.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WooCommerce Contact Form 7 Enquiry
+Plugin URI: devnecks.com
+Description: This plugin adds Contact Form 7 enquiry WooCommerce prosucts
+Author: venqka
+Author URI: devnecks.com
+Version: 1.0
+*/
+

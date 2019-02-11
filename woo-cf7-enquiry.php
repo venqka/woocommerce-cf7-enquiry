fdff78f6845e0ded9f804b723189836affdad71c
venqka <venelinborisov5@gmail.com>
2019-02-11 11:55:49 +0200

    Disable wcfe product purchase and add contact form 7

--- woo-cf7-enquiry.php
+++ woo-cf7-enquiry.php
@@ -16,3 +16,38 @@ function wcfe_load_text_domain() {
 add_action( 'init', 'wcfe_load_text_domain' );
 
 include( 'wcfe-settings.php' );
+
+function disable_wcfe_product_purchase( $purchasable, $product ) {
+
+	$wcfe_global_state = get_option( 'wcfe_disabled' );
+	$product_id = $product->get_id();
+	$product_wcfe = get_post_meta( $product_id, 'wcfe_disabled', true );
+
+	if( $wcfe_global_state == 'no' && $product_wcfe !== 'yes' ) { 
+
+		$purchasable = false;
+	
+	} else {
+
+		$purchasable = true;
+	}	
+	
+	return $purchasable;
+
+}
+add_filter( 'woocommerce_is_purchasable', 'disable_wcfe_product_purchase', 20, 2 );
+
+function wcfe_after_single_product_summary() {
+
+	$wcfe_global_state = get_option( 'wcfe_disabled' );
+	$product_wcfe = get_post_meta( get_the_ID(), 'wcfe_disabled', true );
+	$cf_id = get_option( 'wcfe_cf7' );
+
+	if( $wcfe_global_state == 'no' && $product_wcfe !== 'yes' ) { 
+
+		echo do_shortcode( '[contact-form-7 id="' . $cf_id . '"]' );
+
+	}
+	
+}
+add_action( 'woocommerce_after_single_product_summary', 'wcfe_after_single_product_summary', 20 );
